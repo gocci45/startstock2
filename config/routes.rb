d25415b9b65878a3d1b16a817b11b57f3cd2d3ce
gotti45 <yuto.yogotti35@icloud.com>
2020-12-24 17:01:09 +0900

    エラーメッセージ導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'purchases/index'
-
+  get '/users', to: 'tops#index'
   root to: "tops#index"
   resources :tops do 
       resources :purchases ,only: [:index , :create]
